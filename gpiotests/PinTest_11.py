7e805951ea8fc2dde4e117904a692b1a196204be
Barnaby Kent <barnabypaulkent@gmail.com>
2012-11-29 00:37:11 +0000

    added python file with functions examples

--- gpiotests/PinTest_11.py
+++ gpiotests/PinTest_11.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-""" Must be run as root - sudo python Blink11.py """
+""" Must be run as root - sudo python PinTest_11.py """
 
 import time, RPi.GPIO as GPIO 
 
 GPIO.setmode(GPIO.BOARD)
+
+#Remember to setup the GPIO pin as input or output 
 GPIO.setup(11, GPIO.OUT)
 
 while True:
 	print "Just about to set output to 0"
-	LEDon = GPIO.output(11, 0)
-	print "Just set output to 0 about to sleep for 1"
+	outputOff = GPIO.output(11, 0)
+	print "Just set output to 0 about to sleep for 1 second"
 	time.sleep(1)
 	print "Just slept after setting output to 1"
-	LEDoff = GPIO.output(11, 1)
-	print "Just set output to 1 just about to sleep for 1"
+	outputOn = GPIO.output(11, 1)
+	print "Just set output to 1 just about to sleep for another 1 second"
 	time.sleep(1)
 
